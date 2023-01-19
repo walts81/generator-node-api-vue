0a06cf5915aba8ddded53b24ad7280418d2bf717
Josh Walters <josh.walters@outlook.com>
2023-01-18 22:43:58 -0600

    renamed template src folder to sourcecode

--- generators/app/index.js
+++ generators/app/index.js
@@ -77,7 +77,7 @@ module.exports = class extends Generator {
     this.fs.copy(this.templatePath('_devcontainer/'), this.destinationPath('.devcontainer/'));
     this.fs.copy(this.templatePath('_vscode/'), this.destinationPath('.vscode/'));
     this.fs.copyTpl(this.templatePath('root/*'), this.destinationRoot(), this.props);
-    this.fs.copyTpl(this.templatePath('src/*'), this.destinationPath('src/'), this.props);
+    this.fs.copyTpl(this.templatePath('sourcecode/*'), this.destinationPath('src/'), this.props);
   }
 
   install() {
