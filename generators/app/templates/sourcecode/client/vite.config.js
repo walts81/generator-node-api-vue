e4c826c3d2252e9f31a12319e1bcf6411cbfbf32
Josh Walters <josh.walters@outlook.com>
2023-08-25 12:12:34 -0500

    updated debugging configs for debugging client and API simultaneously added gitlab-ci config

--- generators/app/templates/sourcecode/client/vite.config.js
+++ generators/app/templates/sourcecode/client/vite.config.js
@@ -33,6 +33,14 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 8000,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000/',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
     },
   };
 });
