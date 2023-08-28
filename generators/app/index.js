40dc837a52afd52f3b3b80c9a1f159a8a3877287
Josh Walters <josh.walters@outlook.com>
2023-08-28 09:44:32 -0500

    removed github getUsername function

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,95 +3,92 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 const _ = require('underscore.string');
-const { github, git } = require('yeoman-generator/lib/actions/user');
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(`Welcome to the terrific ${chalk.red('@walts81/node-api-vue')} generator!`));
+    
+    const prompts = [
+      {
+        type: 'string',
+        name: 'name',
+        message: 'Enter a name for this API:',
+        default: 'My Spectacular Node Api',
+      },
+      {
+        type: 'string',
+        name: 'desc',
+        message: 'Enter a description for this API (optional):',
+        default: '',
+      },
+      {
+        type: 'string',
+        name: 'author',
+        message: `What's your name?`,
+        default: git.name() || '',
+      },
+      {
+        type: 'string',
+        name: 'email',
+        message: `What's your email?`,
+        default: git.email() || '',
+      },
+      {
+        type: 'string',
+        name: 'githubUser',
+        message: 'Enter your github username:',
+        default: username || '',
+      },
+      {
+        type: 'string',
+        name: 'gitUrl',
+        message: 'Enter the url for your git repo (optional):',
+        default: '',
+      },
+      {
+        type: 'list',
+        name: 'browser',
+        message: 'Which browser would you like to use for debugging?',
+        choices: [
+          { name: 'Chrome', value: 'chrome' },
+          { name: 'MS Edge', value: 'msedge' },
+        ],
+        default: 'chrome',
+      },
+      {
+        type: 'confirm',
+        name: 'npmInstall',
+        message: 'Would you like the generator to automatically run `npm install`?',
+        default: false,
+      },
+      {
+        type: 'confirm',
+        name: 'gitInit',
+        message: 'Would you like the generator to initialize a git repo?',
+        default: false,
+      },
+    ];
 
-    return github.username().then(username => {
-      const prompts = [
-        {
-          type: 'string',
-          name: 'name',
-          message: 'Enter a name for this API:',
-          default: 'My Spectacular Node Api',
-        },
-        {
-          type: 'string',
-          name: 'desc',
-          message: 'Enter a description for this API (optional):',
-          default: '',
-        },
-        {
-          type: 'string',
-          name: 'author',
-          message: `What's your name?`,
-          default: git.name() || '',
-        },
-        {
-          type: 'string',
-          name: 'email',
-          message: `What's your email?`,
-          default: git.email() || '',
-        },
-        {
-          type: 'string',
-          name: 'githubUser',
-          message: 'Enter your github username:',
-          default: username || '',
-        },
-        {
-          type: 'string',
-          name: 'gitUrl',
-          message: 'Enter the url for your git repo (optional):',
-          default: '',
-        },
-        {
-          type: 'list',
-          name: 'browser',
-          message: 'Which browser would you like to use for debugging?',
-          choices: [
-            { name: 'Chrome', value: 'chrome' },
-            { name: 'MS Edge', value: 'msedge' },
-          ],
-          default: 'chrome',
-        },
-        {
-          type: 'confirm',
-          name: 'npmInstall',
-          message: 'Would you like the generator to automatically run `npm install`?',
-          default: false,
-        },
-        {
-          type: 'confirm',
-          name: 'gitInit',
-          message: 'Would you like the generator to initialize a git repo?',
-          default: false,
-        },
-      ];
-
-      return this.prompt(prompts).then(props => {
-        // To access props later use this.props.someAnswer;
-        props.slugged = _.slugify(props.name);
-        props.capSlugged = props.slugged.toUpperCase();
-        props.title = _.titleize(props.name);
-        props.namePrefix = !!props.githubUser ? '@' + props.githubUser + '/' : '';
-        props.fullSlugged = props.namePrefix + props.slugged;
-        props.year = new Date().getFullYear().toString();
-        if (props.author.indexOf(' ') > -1) {
-          const names = props.author.split(' ');
-          props.firstName = names.length > 0 ? names[0].trim() : '';
-          props.lastName = names.length > 1 ? names[1].trim() : '';
-        } else {
-          props.firstName = props.author || '';
-          props.lastName = '';
-        }
-        props.gitUrl = props.gitUrl || '';
-        this.props = props;
-        this.destinationRoot(props.slugged);
-      });
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      props.slugged = _.slugify(props.name);
+      props.capSlugged = props.slugged.toUpperCase();
+      props.title = _.titleize(props.name);
+      props.namePrefix = !!props.githubUser ? '@' + props.githubUser + '/' : '';
+      props.fullSlugged = props.namePrefix + props.slugged;
+      props.year = new Date().getFullYear().toString();
+      if (props.author.indexOf(' ') > -1) {
+        const names = props.author.split(' ');
+        props.firstName = names.length > 0 ? names[0].trim() : '';
+        props.lastName = names.length > 1 ? names[1].trim() : '';
+      } else {
+        props.firstName = props.author || '';
+        props.lastName = '';
+      }
+      props.gitUrl = props.gitUrl || '';
+      this.props = props;
+      this.destinationRoot(props.slugged);
     });
   }
 
